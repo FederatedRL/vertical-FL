a6698298b4f396fc39bcc00c7e4635f63a8d2584
lavendermo <shuangzh_5@163.com>
2020-07-22 16:17:24 +0800

    fix loss bug

--- federated learning/vertical FL.py
+++ federated learning/vertical FL.py
@@ -4,7 +4,8 @@ import torch.optim as optim
 from torch.autograd import Variable
 import pandas as pd
 import numpy as np
-from torch.utils.data import DataLoader,TensorDataset
+from torch.utils.data import DataLoader, TensorDataset
+
 
 class BottomModelA(nn.Module):
     def __init__(self):
@@ -16,6 +17,7 @@ class BottomModelA(nn.Module):
         x = self.layer1(x)
         x = self.layer2(x)
         return x
+
 
 class BottomModelB(nn.Module):
     def __init__(self):
@@ -69,9 +71,9 @@ if __name__ == '__main__':
     optimizer4 = optim.SGD(bottom_model_B.parameters(), lr=0.01, momentum=0.9)
 
     guest_data = pd.read_csv('give_credit_a.csv', usecols=['x0', 'x1', 'x2', 'x3', 'x4'])
-    guest_data = torch.from_numpy(np.array(guest_data,dtype='float32'))
+    guest_data = torch.from_numpy(np.array(guest_data, dtype='float32'))
     host_data = pd.read_csv('give_credit_b.csv', usecols=['x0', 'x1', 'x2', 'x3', 'x4'])
-    host_data = torch.from_numpy(np.array(host_data,dtype='float32'))
+    host_data = torch.from_numpy(np.array(host_data, dtype='float32'))
     label = pd.read_csv('give_credit_b.csv', usecols=['y'])
     label = np.array(label).reshape(150000)
     label = torch.from_numpy(label)
@@ -85,14 +87,14 @@ if __name__ == '__main__':
     for epoch in range(num_of_epochs):
         running_loss = 0.0
         running_corrrect = 0
-        print("Epoch  {}/{}".format(epoch+1, num_of_epochs))
+        print("Epoch  {}/{}".format(epoch + 1, num_of_epochs))
         host_iter = iter(host_loader)
         guest_iter = iter(guest_loader)
         while True:
             try:
                 dataA = guest_iter.next()[0]
                 dataA = Variable(dataA)
-                dataB,data_label = host_iter.next()
+                dataB, data_label = host_iter.next()
                 dataB = Variable(dataB)
                 data_label = Variable(data_label)
             except StopIteration:
@@ -104,20 +106,20 @@ if __name__ == '__main__':
 
             PartyA_output = bottom_model_A(dataA)
             PartyB_output = bottom_model_B(dataB)
-            Interactive_output = interactive(PartyA_output,PartyB_output)
+            Interactive_output = interactive(PartyA_output, PartyB_output)
             output = top_model(Interactive_output)
             _, pred = torch.max(output.data, 1)
-            loss = criterion(output,data_label)
+            loss = criterion(output, data_label)
             loss.backward()
             optimizer1.step()
             optimizer2.step()
             optimizer3.step()
             optimizer4.step()
             running_loss += float(loss.data)
-            #print("label:",data_label)
-            #print("prediction:",pred)
+            # print("label:",data_label)
+            # print("prediction:",pred)
             running_corrrect += torch.sum(pred == data_label)
 
-        print("correct:",int(running_corrrect))
+        print("correct:", int(running_corrrect))
         print("Loss is {:.4f}, Train Accuracy is:{:.4f}%"
               .format(running_loss / len(host_data), 100 * int(running_corrrect) / len(host_data)))
--- vertical federated learning(new).py
+++ vertical federated learning(new).py
@@ -4,8 +4,9 @@ import torch.optim as optim
 from torch.autograd import Variable
 import pandas as pd
 import numpy as np
-from torch.utils.data import DataLoader,TensorDataset
+from torch.utils.data import DataLoader, TensorDataset
 import matplotlib.pyplot as plt
+
 
 class VerticalFL_Model(nn.Module):
     def __init__(self):
@@ -15,12 +16,12 @@ class VerticalFL_Model(nn.Module):
         self.interactive_layerA = nn.Linear(10, 15)
         self.interactive_layerB = nn.Linear(10, 15)
         self.interactive_activation_layer = nn.ReLU(True)
-        self.top_layer = nn.Sequential(nn.Linear(15, 10), nn.ReLU(True),nn.Linear(10, 2))
+        self.top_layer = nn.Sequential(nn.Linear(15, 10), nn.ReLU(True), nn.Linear(10, 2))
 
-    def forward(self,inputA,inputB):
+    def forward(self, inputA, inputB):
         PartyA_output = self.bottom_layerA(inputA)
         PartyB_output = self.bottom_layerB(inputB)
-        interactive_activation_input = self.interactive_layerA(PartyA_output)+self.interactive_layerB(PartyB_output)
+        interactive_activation_input = self.interactive_layerA(PartyA_output) + self.interactive_layerB(PartyB_output)
         interactive_output = self.interactive_activation_layer(interactive_activation_input)
         output = self.top_layer(interactive_output)
         return output
@@ -31,7 +32,7 @@ if __name__ == '__main__':
     criterion = torch.nn.CrossEntropyLoss()
     optimizer = optim.SGD(model.parameters(), lr=0.01, momentum=0.9)
 
-    #data preprocessing
+    # data preprocessing
     guest_data = pd.read_csv('give_credit_a.csv', usecols=['x0', 'x1', 'x2', 'x3', 'x4'])
     guest_data = np.array(guest_data, dtype='float32')
     guest_train_data = torch.from_numpy(guest_data[:120000])
@@ -45,7 +46,7 @@ if __name__ == '__main__':
     train_label = torch.from_numpy(label[:120000])
     test_label = torch.from_numpy(label[120000:])
 
-    #construct datasets
+    # construct datasets
     host_train_dataset = TensorDataset(host_train_data, train_label)
     host_train_loader = DataLoader(dataset=host_train_dataset, batch_size=64, shuffle=False)
     host_test_dataset = TensorDataset(host_test_data, test_label)
@@ -55,7 +56,7 @@ if __name__ == '__main__':
     guest_test_dataset = TensorDataset(guest_test_data)
     guest_test_loader = DataLoader(dataset=guest_test_dataset, batch_size=64, shuffle=False)
 
-    #train the model
+    # train the model
     num_of_epochs = 5
     for epoch in range(1, num_of_epochs + 1):
         running_loss = 0.0
@@ -63,7 +64,7 @@ if __name__ == '__main__':
         print("Epoch  {}/{}".format(epoch, num_of_epochs))
         host_train_iter = iter(host_train_loader)
         guest_train_iter = iter(guest_train_loader)
-        #loss_list = []
+        # loss_list = []
         while True:
             try:
                 train_dataA = guest_train_iter.next()[0]
@@ -75,14 +76,14 @@ if __name__ == '__main__':
                 break
             optimizer.zero_grad()
 
-            output = model(train_dataA,train_dataB)
+            output = model(train_dataA, train_dataB)
             _, pred = torch.max(output.data, 1)
             loss = criterion(output, train_data_label)
             loss.backward()
             optimizer.step()
-            running_loss += float(loss.data)
-            #print("loss:", float(loss.data))
-            #loss_list.append(float(loss.data))
+            running_loss += float(loss.data) * train_dataA.size(0)
+            # print("loss:", float(loss.data))
+            # loss_list.append(float(loss.data))
             # print("label:",data_label)
             # print("prediction:",pred)
             running_correct += torch.sum(pred == train_data_label)
@@ -90,7 +91,7 @@ if __name__ == '__main__':
         print("Loss is {:.4f}, Train Accuracy is:{:.4f}%"
               .format(running_loss / len(host_train_data), 100 * int(running_correct) / len(host_train_data)))
 
-    #evaluation
+    # evaluation
     testing_correct = 0
     host_test_iter = iter(host_test_loader)
     guest_test_iter = iter(guest_test_loader)
